abd979ac52b149c8d09e5b9c93aa31b22c27626d
Rukmani Tiwari <rukmani.tiwari@datastax.com>
2022-10-18 14:02:20 -0700

    Added the Formula

---
+++ Formula/homebrew-aws-okta.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewAwsOkta < Formula
+  desc "aws-vault like tool for Okta authentication"
+  homepage ""
+  url "https://github.com/RukmaniDT/homebrew-aws-okta/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "8e06ea8297e20e859c2cb09a2a18d6a4776be4122f360bb2a753798080c2fa04"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-aws-okta`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
